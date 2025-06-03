bf60dea596d778605da9c4b2c8589d1e4fb04c27
Mythezone <mythezone@gmail.com>
2025-06-03 21:57:28 +0800

    Refactor code structure for improved readability and maintainability

--- core/base.py
+++ core/base.py
@@ -1,19 +1,6 @@
-from __future__ import annotations
+# from __future__ import annotations
 from ast import Raise
 import numpy as np
-
-
-# class Singleton:
-#     _instances = {}
-
-#     def __new__(cls, *args, **kwargs):
-#         if cls not in cls._instances:
-#             instance = super(Singleton, cls).__new__(cls)
-#             instance.__init__(*args, **kwargs)
-#             instance._initialized = True
-#             cls._instances[cls] = instance
-
-#         return cls._instances[cls]
 
 
 class Singleton(type):
