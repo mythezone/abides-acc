250cfd0792f01a5d00b5a562b119b8fa5bdc41a9
Mythezone <mythezone@gmail.com>
2025-11-11 00:28:01 +0800

    Add copyright documents.

--- core/symbol.py
+++ core/symbol.py
@@ -61,56 +61,10 @@ class Symbol:
                 self.real_history[self.real_history["日期"] == date].iloc[0].to_dict()
             )
 
-        # if name in Symbol._symbol_dict:
-        #     return
-        # self.name = name
-        # self.r_bar = r_bar
-        # self.kappa = kappa
-        # self.sigma_s = sigma_s
-        # self.fund_vol = fund_vol
-        # self.megashock_lambda_a = megashock_lambda_a
-        # self.megashock_mean = megashock_mean
-        # self.megashock_var = megashock_var
-
-    #     Symbol._symbol_dict[name] = self
-    #     Symbol._symbol_name_list.append(name)
-
-    # def __str__(self):
-    #     return f"{self.name}: r_bar={self.r_bar}, kappa={self.kappa}, sigma_s={self.sigma_s}, fund_vol={self.fund_vol}, megashock_lambda_a={self.megashock_lambda_a}, megashock_mean={self.megashock_mean}, megashock_var={self.megashock_var}"
-
-    # def __repr__(self):
-    #     return f"{self.name}: r_bar={self.r_bar}"
-
-    # @classmethod
-    # def get_symbol_by_name(cls, name):
-    #     if name in cls._symbol_dict:
-    #         return cls._symbol_dict[name]
-    #     else:
-    #         raise ValueError(f"Symbol with name {name} not found.")
-
-    # @classmethod
-    # def __class_getitem__(cls, name):
-    #     return cls.get_symbol_by_name(name)
-
-    # @classmethod
-    # def size(cls):
-    #     return len(cls._symbol_dict)
-
-    # @staticmethod
-    # def get_random_symbol():
-    #     return random.choice(Symbol._symbol_dict.values())
-
-    # @staticmethod
-    # def __len__():
-    #     return len(Symbol._symbol_dict)
-
-
 class EFT:
     def __init__(self, portfolio: List[Symbol]):
         self.portfolio = portfolio
 
-
 if __name__ == "__main__":
-    # Example usage
     symbol = Symbol("000001")
     print(symbol.get_real_ohlc("2005-05-09"))  # Should return None values for this date
